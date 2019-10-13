fc407f8d10fe7a1061b44b31bd896b44e4134c3a
spacejack <spacejack@gmail.com>
2019-10-13 13:39:08 -0400

    Update packages

--- src/lib/dom.ts
+++ src/lib/dom.ts
@@ -1,42 +1,14 @@
 // DOM utils
 
-/** The transform style prefix used by this browser (if any) */
-export const transformStylePrefix = (function() {
-	function getAttrLc (e: Element, attr: string) {
-		const s = e.getAttribute(attr)
-		return (!!s && typeof s === 'string') ? s.toLowerCase() : ''
-	}
-	const t = 'translate3d(100px,20px,0px)' // the transform we'll use to test
-	const el = window.document.createElement('div') // Make a test element
-
-	//  Check support for current standard first
-	el.style.transform = t
-	let style = getAttrLc(el, 'style')
-	if (style.indexOf('transform') === 0) return '' // current - done
-
-	//  Try beta/prefixed names...
-	el.style['MozTransform'] = t // firefox
-	el.style['webkitTransform'] = t // webkit/chrome
-	el.style['msTransform'] = t // IE
-	style = getAttrLc(el, 'style')
-
-	if (style.indexOf('moz') >= 0) return 'moz'
-	if (style.indexOf('webkit') >= 0) return 'webkit'
-	if (style.indexOf('ms') >= 0) return 'ms'
-	console.warn('3D transform styles may not be supported by this browser')
-	return ''
-}())
-
-export const transformStyleName = transformStylePrefix.length > 0
-	? '-' + transformStylePrefix + '-transform'
-	: 'transform'
-
-export function getCssRule (sel: string) {
-	const rules = window.document.styleSheets[0]['cssRules'] || document.styleSheets[0]['rules']
+/** Finds a CSS Rule by name */
+export function getCssRule (sel: string): CSSStyleRule | undefined {
+	const sheet = window.document.styleSheets[0] as CSSStyleSheet
+	const rules = sheet.cssRules || sheet.rules
 	for (let i = 0, n = rules.length; i < n; ++i) {
-		if (rules[i].selectorText === sel) return rules[i]
+		const rule = rules[i] as CSSStyleRule
+		if (rule.selectorText === sel) return rule
 	}
-	return null
+	return undefined
 }
 
 /**
@@ -44,15 +16,15 @@ export function getCssRule (sel: string) {
  */
 export function setCssStyle (sel: string, style: string, val: string) {
 	const rule = getCssRule(sel)
-	if (!rule) return
-	rule.style[style] = val
+	if (rule != null) {
+		(rule.style as any)[style] = val
+	}
 }
 
 /**
  * Ensures this DOM element is rendered and ready so that CSS animations can be applied.
  */
 export function readyDom (dom: Element) {
-	// Assign value to a variable for side-effects.
 	// Reading from the DOM element ensures it is rendered.
-	let temp = (dom as HTMLElement).offsetHeight /* tslint:disable-line no-unused-variable */
+	const temp = dom.getBoundingClientRect() /* tslint:disable-line no-unused-variable */
 }
